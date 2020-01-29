9faa0bf35341142fe901ea46a06c132630562b84
cexbrayat <cedric@ninja-squad.com>
2020-01-29 14:29:21 +0100

    feat: use alpha.3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    Hello
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue';
 
-export default Vue.extend({
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+export default defineComponent({
+  name: 'app'
 });
 </script>
 
